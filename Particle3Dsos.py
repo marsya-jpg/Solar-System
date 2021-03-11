0f64c65bd7ce1c137f293f56134e4a80d710c41a
marsya-jpg <72673289+marsya-jpg@users.noreply.github.com>
2021-03-11 05:44:45 +0000

    Add files via upload

---
+++ Particle3Dsos.py
@@ -0,0 +1,160 @@
+"""
+PARTICLE3D: Class to describe N bodies in solar system 
+
+ An instance describes a particle in Euclidean 3D space: 
+ velocity and position are [3] arrays
+
+ Includes time integrator methods +...
+
+author: Marsya Irdina Mohammad Karim s1938504
+
+"""
+import sys
+import math
+import numpy as np
+    
+
+class Particle(object):
+    """
+    Class to describe point-particles in 3D space
+
+        Properties:
+    label: name of the body
+    mass: mass of the body
+    pos: position of the body
+    vel: velocity of the body
+
+        Methods:
+    __init__
+    __str__
+    kinetic_e  - computes the kinetic energy
+    momentum - computes the linear momentum
+    leap_pos2nd - updates the position to 2nd order
+    leap_vel - updates the velocity
+
+        Static Methods:
+    new_particle - initializes a P3D instance from a file handle
+    sys_kinetic - computes total K.E. of a p3d list
+    com_velocity - computes total mass and CoM velocity of a p3d list
+    """
+    def __init__(self, label, mass, pos, vel):
+        """
+        Initialises a particle in 3D space
+
+        :param label: String w/ the name of the particle
+        :param mass: float, mass of the particle
+        :param position: [3] float array w/ position
+        :param velocity: [3] float array w/ velocity
+        """
+        
+        self.label = label
+        self.mass = mass
+        self.vel = vel
+        self.pos = pos
+        
+
+    @staticmethod
+    def new_particle(line):
+        #Splits every element in the line into list called data
+        data = line.split(" ")
+        label = str(data[0])
+        mass = float(data[1])
+        pos = np.array(data[2:5],float)
+        vel = np.array(data[5:],float)
+                  
+        return Particle(label, mass, pos, vel)
+       
+    def __str__(self):
+        """
+        Define output format.
+        Prints out mass, D, re, alpha
+        For particle p=(2.0, 0.5, 1.0) this will print as
+        "x = 2.0, v = 0.5, m = 1.0"
+        """
+        
+        return "x = " + str(self.pos) + ", v = " + str(self.vel) + ", m = " + str(self.mass)
+    
+    #INSTANCE
+    def KE(self):
+        """
+        Returns the kinetic energy of a body
+
+        :return ke: float, 1/2 m v**2
+        """
+        
+        return 0.5*self.mass*np.linalg.norm(self.vel)**2
+    
+    def momentum(self):
+        """
+        Returns the linear momentum of a Particle3D instance
+        :return p: (3) float np.array, m*v
+        """
+        return self.mass*self.velocity
+
+    def leap_pos2nd(self, dt, force):
+        """
+        2nd order position update
+
+        :param dt: timestep
+        :param force: [3] float array, the total force acting on the particle
+        """  
+        self.pos += dt*self.vel + (dt**2)*force/(2*self.mass)
+        return self.pos 
+        
+    def leap_vel(self, dt, force):
+        """
+        Velocity update
+
+        :param dt: timestep
+        :param force: [3] float array, the total force acting on the particle
+        """    #Adds a factor to the current value of self.velocity
+        self.vel += dt*force/self.mass
+        
+        return self.vel
+    
+    @staticmethod
+    def sys_kinetic(p_list):
+        """
+        Returns the kinetic energy of the whole system in a list
+
+        :param p3d_list: list in which each item is a P3D instance
+
+        :return sys_ke: \sum 1/2 m_i v_i^2 
+        """
+        sys_ke = 0
+        for particle in p_list:
+            sys_ke += particle.KE()
+            
+        return sys_ke
+
+
+    @staticmethod
+    def com_velocity(p_list):
+        """
+        Computes the total mass and CoM velocity of a list of P3D's
+
+        :param p3d_list: list in which each item is a P3D instance
+        :return total_mass: The total mass of the system in
+                            array of size N x 1
+        :return com_vel: Centre-of-mass velocity in
+        """
+        N = len(p_list)
+        sum_mom = np.zeros([3])
+        total_mass = 0
+        
+        
+        for particle in p_list:
+            total_mass += particle.mass
+            sum_mom += particle.mass*particle.vel
+            
+        com_vel = sum_mom/total_mass    
+        return total_mass, com_vel
+    
+    
+  
+
+
+
+
+
+
---
+++ velocityVerletsos.py
@@ -0,0 +1,306 @@
+"""
+VELOCITY VERLET: velocity Verlet time integration of
+a body moving in orbit
+
+Produces plots of the position of the particle
+and its energy, both as function of time. Also
+saves both to file.
+
+The potential is V(x) = a*x^4 - b*x^2, where
+a and b are hard-coded in the main() method
+and passed to the functions that
+calculate force and potential energy.
+"""
+
+import sys
+import numpy as np
+import matplotlib.pyplot as pyplot
+from Particle3Dsos import Particle 
+from scipy.signal import find_peaks, peak_prominences
+
+def get_separation(p_list, N):
+    """
+    Method that calculates all necessary separations to return as an array
+    
+    :param p_list : list
+    :return: N x N array
+    """
+    mod_array = np.zeros([N,N])
+    separations = np.zeros([N,N,3])
+    
+    for i in range(N):
+        for j in range(N):
+            separations[i,j] = p_list[i].pos - p_list[j].pos
+            mod_array[i,j] = np.linalg.norm(separations[i,j])
+            
+    return mod_array, separations    
+    
+
+def get_force(p_list, G, N):
+    """
+    Returns array of 3D vector of force from all pair interactions
+    """
+
+    sys_force = np.zeros([N,3])
+    #Addresses tuple from get_separation
+    sep, sep_vector = get_separation(p_list, N)
+    force = np.zeros([N,N,3])
+    
+    for i in range(N):
+        for j in range(N):
+            #Gets the force numerator
+            if i!=j:      
+                force[i,j] = -G*p_list[i].mass*p_list[j].mass/(sep[i,j])**3 * sep_vector[i,j]
+                
+            else:
+                continue
+            
+    sys_force = force.sum(axis=1)
+    
+    return sys_force
+
+def update_vel(p_list, force, dt, N):
+    """
+    Velocity updater for a list of P3Ds
+    """
+    
+    sys_velocity = np.zeros([N,3])
+    for i in range(N):
+        f = force[i]
+        sys_velocity[i] = p_list[i].leap_vel(dt, f)
+        
+    return sys_velocity    
+        
+def update_pos(p_list, force, dt, N):
+    """
+    2nd order position updater for a list of P3Ds
+    Uses dynamic methods for velocity and position as the velocity and ]
+    position changes
+    """
+    sys_position = np.zeros([N,3])
+    #Opens [N,N] array to hold pair separation values in scalar
+    for i in range(N):
+        f = force[i]
+        sys_position[i] = p_list[i].leap_pos2nd(dt, f)
+        
+    
+    return sys_position
+    
+def get_pos(p_list, N):
+    """
+    Get p_list[i].pos in an array
+    """
+    sys_position = np.zeros([N,3])
+    sys_mod = np.zeros([N])
+    for i in range(N):
+        sys_position[i] = p_list[i].pos
+        sys_mod[i] = np.linalg.norm(p_list[i].pos)
+        
+    return sys_position, sys_mod
+    
+def sys_pot_energy(p_list, G, dt, N):
+    """
+    Returns potential energy of system as a single value {scalar}
+    Sum of potential from every interaction between bodies
+    """
+    potential = np.zeros([N,N])
+    sep = get_separation(p_list, N)[0]
+    for i in range(N):
+        for j in range(N):
+            if i!=j:
+                potential[i,j] = -G*p_list[i].mass*p_list[j].mass/sep[i,j]
+            else:
+                continue
+            
+    sys_potential= np.triu(potential).sum()
+    return sys_potential
+
+     
+
+# Begin main code
+def main():
+    # Read name of output file from command line
+    if len(sys.argv)!=2:
+        print("Wrong number of arguments.")
+        print("Usage: " + sys.argv[0] + " <output file>")
+        sys.exit()
+    else:
+        outfile_name = sys.argv[1]
+
+    # Open output file
+    outfile = open(outfile_name, "w")
+    #Opens o2_vibration in symplectic and verlet
+    
+    #Reads in simulation parameters 
+    with open("sim_parameter.txt","r") as my_input:
+        #Reads first line only
+        solar_data = my_input.readline().split(" ")
+        G = float(solar_data[0])
+        dt = float(solar_data[1])
+        numstep = int(solar_data[2])        
+        
+    #Set up celestial bodies from input file
+    f = open("p_setup.txt")
+    p_list = []
+    
+    
+   #Assigns new particle from input particle file
+    count = sum(1 for line in open('p_setup.txt'))
+    print(count)
+    for i in range(count):
+        val = f.readline()
+        part = Particle.new_particle(val)
+        p_list.append(part)      
+   
+    f.close()
+    
+    N = len(p_list)
+       
+    #COM  correction
+    total_mass, com_vel = Particle.com_velocity(p_list)
+    for i in range(N):
+        p_list[i].vel = p_list[i].vel-com_vel
+    
+    #Setup initial condition
+    time = 0
+    
+    # Write out initial conditions
+    energy = sys_pot_energy(p_list, G, dt, N) + Particle.sys_kinetic(p_list)
+    force = get_force(p_list, G, N)
+    KE = Particle.sys_kinetic(p_list)
+    time_list = [time]
+    sys_pos, sys_mod = get_pos(p_list, N)
+        
+    # Initialise observables initial conditions and data list
+    mod_array, seps = get_separation(p_list, N)
+    
+    planet_sep = np.zeros([numstep+1, N-2])
+    aphelion = []
+    perihelion = []
+    
+    moon_sep = np.zeros([numstep+!])
+    apogee = []
+    perigee = []
+    
+    y_max = np.zeros([numstep+1, N-1])
+
+    energy_list = [energy]
+    ke_list = [KE]
+    force_list = [np.linalg.norm(force[9])]
+    y_max[0] = sys_mod[1:12]
+    planet_sep[0] = mod_array[1:-1,0]
+    moon_sep[0] = mod_array[-1,]
+    
+    #Start write XYZ file 
+    #Makes a list of particle names
+    particle_label = [p_list[i].label for i in range(len(p_list))]
+        
+    def xyz(p_list, num):
+        outfile.write("%s\n"%len(p_list))
+        outfile.write("%s\n"%num)
+        for x, particle in zip(sys_pos, particle_label):
+            outfile.write("%s %.18g %.18g %.18g\n"%(particle, x[0], x[1], x[2]))      
+    
+    xyz(p_list, 0)
+    
+    # Start the time integration loop
+
+    for num in range(numstep):
+        # Update particle position
+        update_pos(p_list, force, dt, N)
+        
+        # Update force
+        force_new = get_force(p_list, G, N)
+        
+        # Update particle velocity by averaging
+        # current and new forces
+        update_vel(p_list, 0.5*(force+force_new), dt, N)
+        
+        sys_pos, sys_mod = get_pos(p_list, N)
+        
+        #Assign separations for apsis and periapsis
+        mod_array, seps = get_separation(p_list, N)
+        planet_sep[num+1] = mod_array[1:-1,0]
+        moon_sep[num+1] = mod_array[-1,3]
+
+        # Re-define force value
+        force = force_new
+        
+        KE = Particle.sys_kinetic(p_list)
+
+        # Output particle information
+        energy = sys_pot_energy(p_list, G, dt, N) + Particle.sys_kinetic(p_list)
+        
+        # Increase time
+        time += dt
+
+                                                                     
+        #Writing XYZ file (track.dat)
+        xyz(p_list, num+1)
+        
+        # Append information to data lists
+        time_list.append(time)
+        #pos_list[num+1] = np.linalg.norm(get_pos(p_list, N)[i])
+                            
+        
+        energy_list.append(energy)
+        ke_list.append(KE)
+        
+        force_list.append(np.linalg.norm(force[4]))
+        
+    #print("ps", planet_sep.shape)   (366,10)
+
+    # Post-simulation:
+    # Close output file
+    outfile.close()
+    
+    aphelion.append(np.amax(planet_sep,axis=0))
+    perihelion.append(np.amin(planet_sep, axis=0))
+    print("aphelion of planets",aphelion)
+    print("perihelion of planets", perihelion)
+    
+    apogee.append(np.amax(moon_sep))
+    perigee.append(np.amin(moon_sep))
+    print("apogee of moon", apogee)
+    print("perigee of moon", perigee)  
+    
+    
+    #Calculation of orbital period
+    #Finding peaks
+    period = []
+    
+    #Range of 0 to len(p_list)-1
+    for i in range(0,10):
+        
+        #peak_list = []
+        peaks = find_peaks(planet_sep[:,i], height=0)  #Peaks of position
+        print("peaks",peaks[1])
+        #height = peaks[1] #list of height of peaks
+        peak_time = [time_list[i] for i in peaks[0]] 
+        print("time", peak_time)
+        
+        #prominences = peak_prominences(peaks,time)
+        
+        #print("prominences",prominences)
+        #print("prominences", prominences)
+        val = peak_time[1]-peak_time[0]
+        period.append(val)
+    
+    print("period",period)
+    
+    # Plot particle energy to screen
+    #pyplot.title('Velocity Verlet: total energy vs time')
+    pyplot.title('Velocity Verlet: mercury vs time')
+    pyplot.xlabel('Time')
+    pyplot.ylabel('Mercury')
+    pyplot.plot(time_list, force_list)
+    #pyplot.plot(time_list, pos_list_planet)
+    #pyplot.plot(time_list, planet_progress)
+    #pyplot.plot(time_list, ke_list)
+    pyplot.show()
+
+
+# Execute main method, but only when directly invoked
+if __name__ == "__main__":
+    main()
+
